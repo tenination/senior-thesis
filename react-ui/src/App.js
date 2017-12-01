32d31ec04434cb4a3f81244750b67f85de26b02f
Tim Ninan <tim.ninan@gmail.com>
2017-12-01 01:23:30 -0600

    changed title of react ui for testing purposes

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to MySong.fm</h1>
+          <h2>brought to you by Carter Duncan, Ross Salge, Tim Ninan</h2>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
